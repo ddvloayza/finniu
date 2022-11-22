a513ca1ea59a5d65c0040cdf629c95e07b5e6bbe
Diego De la vega <diego.delavega@crehana.com>
2022-11-22 01:22:42 -0500

    Changes

--- src/apps/blog/urls.py
+++ src/apps/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, BlogDetailView, MethodVignere
+from .views import HomeView, ServicesView
 from apps.core.views import Error404View
 
 app_name = 'blog'
@@ -7,14 +7,11 @@ app_name = 'blog'
 urlpatterns = [
 
     path('', HomeView.as_view(), name='home'),
-    path('blog/<slug:slug>/detail/',
-         BlogDetailView.as_view(),
-         name='blog_detail'),
-    path('blog/404/',
+    path('servicios/',
+         ServicesView.as_view(),
+         name='servicios'),
+    path('404/',
          Error404View.as_view(),
          name='error-404'),
-    path('method-vignere/',
-         MethodVignere.as_view(),
-         name='method_vignere'),
 
 ]
--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -22,22 +22,7 @@ class HomeView(TemplateView):
         return context
 
 
-class BlogDetailView(DetailView):
-    queryset = BlogArticle.objects.all()
-    template_name = 'single-blog.html'
-
-    def get_context_data(self, **kwargs):
-        article = BlogArticle.objects.get(slug=self.kwargs["slug"])
-        context = super().get_context_data(**kwargs)
-        context.update(
-            {
-                "article": article,
-            }
-        )
-        return context
-
-
-class MethodVignere(TemplateView):
-    template_name = 'metodo-vignere.html'
+class ServicesView(TemplateView):
+    template_name = 'servicios.html'
 
     
